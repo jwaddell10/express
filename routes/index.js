0b0fed9a6e93636028b66fe1ce2022ab6ed9fd68
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2024-04-23 15:24:54 -0500

    commit

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,22 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+
+router.get('/users', (req, res) => {
+  return res.send('GET HTTP method on user resource');
+});
+
+router.post('/users', (req, res) => {
+  return res.send('POST HTTP method on user resource');
+});
+
+router.put('/users', (req, res) => {
+  return res.send('PUT HTTP method on user resource');
+});
+
+router.delete('/users', (req, res) => {
+  return res.send('DELETE HTTP method on user resource');
 });
 
 module.exports = router;
